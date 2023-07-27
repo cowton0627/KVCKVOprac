19ffaa516aa1e6e49f8d9afb8ec2a2d99617f53f
Charles Cheng <charles_cheng@microjet.com.tw>
2023-07-27 16:22:39 +0800

    Initial Commit

---
+++ KVCKVOprac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KVCKVOprac
+//
+//  Created by Chun-Li Cheng on 2023/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
